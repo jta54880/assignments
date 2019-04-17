d7ff67229ad5321d28d4e0bf58dbc8b6182dbe30
Jake Afuvai <jtafuvai@gmail.com>
2019-04-17 16:14:54 -0600

    first edition groceryStore.js

---
+++ exercises/groceryStore/groceryStore.js
@@ -0,0 +1,10 @@
+var shopper = {
+    gender: "Female",
+    age: 35,
+    inStore: true,
+    atRegister: () => "Shopper is Checking Out."
+ }
+
+ shopper.groceryCart = ['Milk', 'Eggs', 'Frozen Pizza']
+
+ console.log(shopper.atRegister())

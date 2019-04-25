6aa393d159fea53f8821c91da82faab0d24af286
Jake Afuvai <jtafuvai@gmail.com>
2019-04-25 08:58:12 -0600

    touch up on mario

--- projects/marioPestControl/index.js
+++ projects/marioPestControl/index.js
@@ -1,12 +1,17 @@
 function handleBaddiesBill() {
+    const coinPerBaddie = {
+        goomba: 5,
+        bobOmbs: 7,
+        cheepCheeps: 11
+    }
     let goombaTotal = document.getElementById("goomba-input").value > 0 ?           document.getElementById("goomba-input").value : 0
     let bobombsTotal = document.getElementById("bob-ombs-input").value > 0 ? document.getElementById("bob-ombs-input").value : 0
     let cheepCheepsTotal = document.getElementById("cheep-cheeps-input").value > 0 ? document.getElementById("cheep-cheeps-input").value : 0
-    let baddiesTotal = (goombaTotal * 5) + (bobombsTotal * 7) + (cheepCheepsTotal * 11)
+    let baddiesTotal = (goombaTotal * coinPerBaddie.goomba) + (bobombsTotal * coinPerBaddie.bobOmbs) + (cheepCheepsTotal * coinPerBaddie.cheepCheeps)
     document.getElementById("bill-total").innerText = `${baddiesTotal} Coins` 
-    document.getElementById("goomba-breakdown").innerText = `${goombaTotal} Goomba's @ 5 Coins/Each`
-    document.getElementById("bobombs-breakdown").innerText = `${bobombsTotal} Bob-omb's @ 7 Coins/Each`
-    document.getElementById("cheepCheep-breakdown").innerText = `${cheepCheepsTotal} Cheep-cheep's @ 11 Coins/Each`
+    document.getElementById("goomba-breakdown").innerText = `Goomba (x ${goombaTotal}) @ 5 Coins/Each`
+    document.getElementById("bobombs-breakdown").innerText = `Bob-omb (x ${bobombsTotal}) @ 7 Coins/Each`
+    document.getElementById("cheepCheep-breakdown").innerText = ` Cheep-cheep's (x ${cheepCheepsTotal}) @ 11 Coins/Each`
     document.getElementById("goomba-input").value = null
     document.getElementById("bob-ombs-input").value = null
     document.getElementById("cheep-cheeps-input").value = null

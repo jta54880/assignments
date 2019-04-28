482b6b9d0bd8e9035b1f4fd924c885ed7d1ffcc1
Jake Afuvai <jtafuvai@gmail.com>
2019-04-27 22:23:20 -0600

    working on colossal rpg

--- projects/colossalAdventureConsoleRPG/app.js
+++ projects/colossalAdventureConsoleRPG/app.js
@@ -2,7 +2,7 @@
 function Player(userName) {
     this.userName = userName
     this.hitPoints = 100
-    this.itemInventory = []
+    this.itemInventory = ["Lucky Warrior Pendant"]
     this.attackPower = () => Math.floor(Math.random() * 100 + 20)
     this.didSurviveAttack = false
     this.didDefeatEnemy = false
@@ -27,7 +27,7 @@ function createPlayer(name) {
 console.log(createPlayer("Yim"))
 
 // select a random (out of 3) named enemy
-function selectEnemy() {
+function selectEnemy(ranNum) {
     const enemyArr = ["Boogie", "Woogie", "Moe"]
     let randomNum = Math.floor(Math.random() * 3)
     let enemyName = enemyArr[randomNum]
@@ -38,10 +38,55 @@ function selectEnemy() {
 console.log(selectEnemy())
 
 function randomEnemy() {
-    let randomNum = Math.floor(Math.random() * 3)
-    let newEnemy = selectEnemy()
-    randomNum != 1 ? console.log(`Continue on Your Journey (enter 'w')`) : console.log(`${newEnemy.enemyName} wishes to fight you.`)
-    return newEnemy
+    let randomNum = Math.floor(Math.random() * 2)
+    if (randomNum === 1) {
+        let newEnemy = selectEnemy()
+        return newEnemy
+    } else {
+        return `No tokens here`
+    }
+    
 }
 
 console.log(randomEnemy())
+
+// const ranNum = () => Math.floor(Math.random() * 2)
+
+const readLine = require("readline-sync")
+const {keyInYN, keyInSelect, question, keyIn} = readLine
+
+let gameOver = false
+let gameIntro = "Welcome to the Land of Colossal.  The game of splendor and adventure.  Survive and defeat enemies to earn tokens for your battles.  Your adventure awaits..."
+
+while (!gameOver) {
+    console.log(gameIntro)
+    let response = keyInYN("Do you dare brave the Land of Colossal?")
+    if (!response) {
+        gameOver = true
+        console.log("Perhaps after a few drinks you can muster up enough courage...Until next time!")
+    } else {
+        let userName = question("What is your name, brave warrior? ")
+        const player = createPlayer(userName)
+        let printPlayer = `***********************\n\tPlayer: ${player.userName}
+            User HP: ${player.hitPoints}
+            User Items: ${player.itemInventory.join(", ")}\n***********************\n`
+        let wantToWalk = false
+        while (!wantToWalk) {
+            let response = keyIn(`Would you like to find some tokens? (Enter 'w' to start walking or 'p' to see player info.`, {limit: 'wp'})  
+            if (response==='p') {
+                console.log(printPlayer)
+            } else if (response==='w') {
+                console.log("We're walking!")
+                let enemyApproaching = false
+                // let ranNum = ranNum()
+                
+                    // console.log("An enemy is approaching...")
+                    const enemy = randomEnemy()
+                    
+                    console.log(`Oh No!! It looks like it's ${enemy.enemyName}`)
+                
+                
+            }
+        }
+    }
+}

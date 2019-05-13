d58cfafe31fdd793a7937267985d6622ce7747b7
Jake Afuvai <jtafuvai@gmail.com>
2019-05-13 17:04:41 -0600

    worked on business hpg body... need to start on navbar and footer

---
+++ exercises/business-homepage/src/Main.js
@@ -0,0 +1,34 @@
+import React from "react"
+import ProductCard from "./ProductCard"
+
+const Main = () => {
+    return (
+        <div className="main">
+            <h2>About</h2>
+            <h3>our business</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <br/>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h2>Products</h2>
+            <h3>inventory</h3>
+            <div className="card-container">
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+            </div>
+            <br/>
+            <h2>Customize</h2>
+            <h3>our products</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div className="img-container">
+                <div id="img-one"></div>
+                <div id="img-two"></div>
+                <div id="img-three"></div>
+            </div>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+    )
+}
+
+export default Main
---
+++ exercises/business-homepage/src/ProductCard.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const ProductCard = () => {
+    return (
+        <div className="product-card">
+            <div id="card-img"></div>
+            <h3 id="card-title">Item: Shoes</h3>
+            <h4 id="card-price">$ 49.99</h4>
+            <p id="card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+    )
+}
+
+export default ProductCard
